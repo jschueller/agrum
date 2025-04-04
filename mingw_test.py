80ae32395e4062028c190e15d5c5f9889495d246
Julien Schueller <schueller@phimeca.com>
2025-04-04 14:28:58 +0200

    GH

---
+++ mingw_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import pyagrum as gum
+
+# https://gitlab.com/agrumery/aGrUM/issues/15
+bn = gum.fastBN('a->b->d;a->c->d->e;f->b')
+ie = gum.LazyPropagation(bn)
+ie.makeInference()
+print(ie.posterior('d'))
+
+# https://gitlab.com/agrumery/aGrUM/issues/24
+bn = gum.fastBN('a->b')
+jointe = gum.Tensor().fillWith(1)
+#for i in range(2):
+    #jointe *= bn.cpt(i)
+print(jointe)
+
+# min bug
+bn = gum.fastBN("a->b<-c")
+p = bn.cpt("b")
+print("min=", p.min())
